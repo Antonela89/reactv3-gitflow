e684c503a13a3f057a93d9aac0eda3f66082fdd7
Antonela <antonelaborgogno@gmail.com>
2023-10-07 17:26:30 -0300

    componente api

---
+++ src/componentes/Api.jsx
@@ -0,0 +1,11 @@
+
+const Api = () => {
+    return (
+        <div>
+            <h1>Se va la tercera...</h1>
+            <h2>Ahora probamos con git flow...</h2>
+        </div>
+    )
+}
+
+export default Api
