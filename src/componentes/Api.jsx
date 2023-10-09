b744b9a216cf0ad6cb0d5ac66ed24fd451d5b316
Horaciomdp <horazopo@gmail.com>
2023-10-09 11:28:39 -0300

    a ver que onda

--- src/componentes/Api.jsx
+++ src/componentes/Api.jsx
@@ -4,6 +4,7 @@ const Api = () => {
         <div>
             <h1>Se va la tercera...</h1>
             <h2>Ahora probamos con git flow...</h2>
+            <h1>Que lindo es copiar!!</h1>
         </div>
     )
 }
