cd3a459775079b07efea70a00a8116b36180b481
Horaciomdp <horazopo@gmail.com>
2023-10-09 13:00:37 -0300

    incluimos card

---
+++ src/componentes/Api4.jsx
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Api4 = () => {
+  return (
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src="../../public/assets/img/seraAsi.jpg" />
+    <Card.Body>
+      <Card.Title>Horacio</Card.Title>
+      <Card.Text>
+        Gracias Anto
+      </Card.Text>
+      <Button variant="primary">Alta card amego</Button>
+    </Card.Body>
+  </Card>
+  )
+}
+
+export default Api4;
