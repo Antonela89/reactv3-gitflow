0b0d15b77cb59813b282091e9755091373cedbee
Horaciomdp <horazopo@gmail.com>
2023-10-09 12:17:40 -0300

    frutis

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Api from './componentes/Api.jsx'
 import Api2 from './componentes/Api2.jsx'
+import Api3 from './componentes/Api3.jsx'
 
 function App() {
   return (
     <div className="App">
       <Api/> 
-      <Api2/>     
+      <Api2/>
+      <Api3/>      
     </div>
   );
 }
