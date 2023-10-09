e014deccc2869475ddb8d5e09d7461c47880d51f
MoniCrichton <email@example.comcodeando3@gmail.com>
2023-10-09 11:59:28 -0300

    Cree Componente PRueba

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Api from './componentes/Api.jsx'
+import CompPrueba from './componentes/CompPrueba.jsx'
 
 function App() {
   return (
     <div className="App">
-      <Api/>      
+      <Api/>   
+      <CompPrueba/>  
     </div>
   );
 }
--- src/componentes/CompPrueba.jsx
+++ src/componentes/CompPrueba.jsx
@@ -1,12 +1,12 @@
-
-const Api = () => {
+const CompPrueba = () => {
     return (
         <div>
             <h1>Se va la tercera...</h1>
             <h2>Ahora probamos con git flow...</h2>
             <h2>Aca sigo yo!!...</h2>
+            <h2>Robando código!!! jaja...</h2>
         </div>
     )
 }
 
-export default Api
+export default CompPrueba
